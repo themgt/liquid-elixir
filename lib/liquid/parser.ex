fe93388d5028f9359dcb63889e23ede16e2f142f
Antonio Abella <antonioabella@gmail.com>
2018-10-21 07:25:11 -0400

    refactor(Parser): use direct function call instead of `parsec`

--- lib/liquid/parser.ex
+++ lib/liquid/parser.ex
@@ -2,6 +2,8 @@ defmodule Liquid.Parser do
   @moduledoc """
   Transform a valid liquid markup in an AST to be executed by `render`.
   """
+  @inline true
+
   import NimbleParsec
 
   alias Liquid.Combinators.{General, LexicalToken}
@@ -47,114 +49,61 @@ defmodule Liquid.Parser do
           | String.t()
         ]
 
-  defparsec(:liquid_variable, General.liquid_variable())
-  defparsec(:variable_definition, General.variable_definition())
-  defparsec(:variable_name, General.variable_name())
-  defparsec(:quoted_variable_name, General.quoted_variable_name())
-  defparsec(:variable_definition_for_assignment, General.variable_definition_for_assignment())
-  defparsec(:variable_name_for_assignment, General.variable_name_for_assignment())
-  defparsec(:start_tag, General.start_tag())
-  defparsec(:end_tag, General.end_tag())
-  defparsec(:start_variable, General.start_variable())
-  defparsec(:end_variable, General.end_variable())
-  defparsec(:filter_param, General.filter_param())
-  defparsec(:filter, General.filter())
-  defparsec(:filters, General.filters())
-  defparsec(:single_quoted_token, General.single_quoted_token())
-  defparsec(:double_quoted_token, General.double_quoted_token())
-  defparsec(:quoted_token, General.quoted_token())
-  defparsec(:comparison_operators, General.comparison_operators())
-  defparsec(:logical_operators, General.logical_operators())
-  defparsec(:ignore_whitespaces, General.ignore_whitespaces())
-  defparsec(:condition, General.condition())
-  defparsec(:logical_condition, General.logical_condition())
+  defparsec(:variable_definition, General.variable_definition(), inline: @inline)
+  defparsec(:variable_name, General.variable_name(), inline: @inline)
+  defparsec(:variable_definition_for_assignment, General.variable_definition_for_assignment(), inline: @inline)
+  defparsec(:filter, General.filter(), inline: @inline)
+  defparsec(:filters, General.filters(), inline: @inline)
+  defparsec(:comparison_operators, General.comparison_operators(), inline: @inline)
+  defparsec(:condition, General.condition(), inline: @inline)
+  defparsec(:logical_condition, General.logical_condition(), inline: @inline)
 
-  defparsec(:null_value, LexicalToken.null_value())
-  defparsec(:number, LexicalToken.number())
-  defparsec(:value_definition, LexicalToken.value_definition())
-  defparsec(:value, LexicalToken.value())
-  defparsec(:object_property, LexicalToken.object_property())
-  defparsec(:boolean_value, LexicalToken.boolean_value())
-  defparsec(:string_value, LexicalToken.string_value())
-  defparsec(:object_value, LexicalToken.object_value())
-  defparsec(:variable_value, LexicalToken.variable_value())
-  defparsec(:variable_part, LexicalToken.variable_part())
+  defparsec(:value_definition, LexicalToken.value_definition(), inline: @inline)
+  defparsec(:value, LexicalToken.value(), inline: @inline)
+  defparsec(:number, LexicalToken.number(), inline: @inline)
+  defparsec(:object_property, LexicalToken.object_property(), inline: @inline)
+  defparsec(:variable_value, LexicalToken.variable_value(), inline: @inline)
+  defparsec(:variable_part, LexicalToken.variable_part(), inline: @inline)
+
+  defparsec(:cycle_values, Cycle.cycle_values(), inline: @inline)
+  defparsec(:comment, Comment.tag(), inline: @inline)
+  defparsec(:comment_content, Comment.comment_content(), inline: @inline)
+  defparsecp(:raw, Raw.tag(), inline: @inline)
+  defparsecp(:raw_content, Raw.raw_content(), inline: @inline)
+
+  # The tag order affects the parser execution any change can break the app
+  liquid_tag =
+    choice([
+      Raw.tag(),
+      Comment.tag(),
+      If.tag(),
+      If.unless_tag(),
+      For.tag(),
+      Case.tag(),
+      Capture.tag(),
+      Tablerow.tag(),
+      Cycle.tag(),
+      Assign.tag(),
+      Increment.tag(),
+      Decrement.tag(),
+      Include.tag(),
+      Ifchanged.tag(),
+      Generic.else_tag(),
+      Case.when_tag(),
+      If.elsif_tag(),
+      For.break_tag(),
+      For.continue_tag(),
+      EndBlock.tag(),
+      CustomTag.tag()
+    ])
 
   defparsec(
     :__parse__,
     empty()
     |> choice([
-      parsec(:liquid_tag),
-      parsec(:liquid_variable)
-    ])
-  )
-
-  defparsec(:assign, Assign.tag())
-
-  defparsec(:increment, Increment.tag())
-
-  defparsec(:decrement, Decrement.tag())
-
-  defparsec(:cycle_values, Cycle.cycle_values())
-  defparsec(:cycle, Cycle.tag())
-
-  defparsec(:include, Include.tag())
-
-  defparsec(:comment_content, Comment.comment_content())
-  defparsec(:comment, Comment.tag())
-
-  defparsecp(:raw_content, Raw.raw_content())
-  defparsec(:raw, Raw.tag())
-
-  defparsec(:capture, Capture.tag())
-
-  defparsec(:if, If.tag())
-  defparsec(:elsif, If.elsif_tag())
-  defparsec(:unless, If.unless_tag())
-
-  defparsec(:for, For.tag())
-  defparsec(:break_tag, For.break_tag())
-  defparsec(:continue_tag, For.continue_tag())
-
-  defparsec(:ifchanged, Ifchanged.tag())
-
-  defparsec(:tablerow, Tablerow.tag())
-
-  defparsec(:case, Case.tag())
-  defparsec(:when, Case.when_tag())
-
-  defparsec(:else, Generic.else_tag())
-
-  defparsec(:custom, CustomTag.tag())
-
-  defparsec(:end_block, EndBlock.tag())
-
-  defparsec(
-    :liquid_tag,
-    # The tag order affects the parser execution any change can break the app
-    choice([
-      parsec(:raw),
-      parsec(:comment),
-      parsec(:if),
-      parsec(:unless),
-      parsec(:for),
-      parsec(:case),
-      parsec(:capture),
-      parsec(:tablerow),
-      parsec(:cycle),
-      parsec(:assign),
-      parsec(:increment),
-      parsec(:decrement),
-      parsec(:include),
-      parsec(:ifchanged),
-      parsec(:else),
-      parsec(:when),
-      parsec(:elsif),
-      parsec(:break_tag),
-      parsec(:continue_tag),
-      parsec(:end_block),
-      parsec(:custom)
-    ])
+      liquid_tag,
+      General.liquid_variable(),
+    ]), inline: @inline
   )
 
   @doc """
